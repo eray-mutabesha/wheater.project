c1919f9832bc7ae545deee34d4f469846e650441
Francis Hakim BC <francishakimbc@Franciss-MacBook-Air.local>
2023-10-25 17:27:03 +0200

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function search(){
+
+    const span=document.getElementById("meteospan");
+    const inputsearch=document.getElementById("input_search");
+    valer=inputsearch.value;
+    let lienmeteo=`https://api.openweathermap.org/data/2.5/weather?q=${valer}&units=metric&appid=f3f4d962b5eb831efcbccf782e14216e`;
+    
+    fetch(lienmeteo)
+    .then(reponse=>reponse.json())
+     .then(data=>span.innerHTML=data.main.temp);
+    
+    const drap=document.getElementById("drapo");
+    
+    let lien=`https://restcountries.com/v3.1/name/${valer}?fullText=true`;
+    fetch(lien)
+    .then(res=>res.json())
+    .then(data=>drap.src=data[0].flags.svg);
+    
+    const county=document.getElementById("nameofcountry");
+    county.innerHTML=valer;
+}
+
